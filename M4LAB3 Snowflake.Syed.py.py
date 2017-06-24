c64618367c76fc5de7996a7b14eabfe93d4adec1
rozinasyed <syedr2547@student.faytechcc.edu>
2017-06-23 23:32:44 -0600

    CTI-110
    M4LAB3 Snowflake.Syed.py.py

---
+++ M4LAB3 Snowflake.Syed.py.py
@@ -0,0 +1,91 @@
+# snowflake take
+#CTI 110-M4LAB3: Snowflake
+# Syed
+# 6/23/17
+
+import turtle
+
+def main():
+    win = turtle.Screen()
+    win.bgcolor("gray")
+    
+    
+t = turtle.Turtle()
+
+t.color("pink")
+t.pensize(1)
+
+t.speed(6)
+
+
+t.left(90)
+
+for i in range (10):
+    t.forward(100)
+    t.forward(-30)
+    t.left(60)
+    t.forward(30)
+    t.forward(-30)
+    t.right(140)
+    t.forward(30)
+    t.forward(-30)
+    t.left(60)
+    t.forward(-70)
+    t.left(60)
+
+
+t.color("yellow")
+t.pensize(4)
+t.speed(4)
+for i in range (10):
+    t.forward(100)
+    t.forward(-30)
+    t.left(60)
+    t.forward(30)
+    t.forward(-30)
+    t.right(140)
+    t.forward(30)
+    t.forward(-30)
+    t.left(60)
+    t.forward(-70)
+    t.left(60)
+
+
+t.color("green")
+t.pensize(6)
+t.speed(4)
+for i in range (10):
+    t.forward(100)
+    t.forward(-30)
+    t.left(60)
+    t.forward(30)
+    t.forward(-30)
+    t.right(140)
+    t.forward(30)
+    t.forward(-30)
+    t.left(60)
+    t.forward(-70)
+    t.left(60)
+
+
+
+
+
+t.color("red")
+t.pensize(68)
+t.speed(4)
+for i in range (10):
+    t.forward(100)
+    t.forward(-30)
+    t.left(60)
+    t.forward(30)
+    t.forward(-30)
+    t.right(140)
+    t.forward(30)
+    t.forward(-30)
+    t.left(60)
+    t.forward(-70)
+    t.left(60)
+
+
+main()
